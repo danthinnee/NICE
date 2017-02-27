ced1bc4cc9a67e8e57d5327d2c3832ee9f8e07b3
Elisa Danthinne <danthinne.e@husky.neu.edu>
2017-02-27 18:27:56 +0000

    fixed typos

--- cpp/include/cpu_operations.h
+++ cpp/include/cpu_operations.h
@@ -456,9 +456,9 @@ class CpuOperations {
   /// Input Vector 2
   ///
   /// \return
-  /// This sfunction returns a Vector of type bool
+  /// This function returns a Vector of type bool
   static Vector<bool> LogicalAnd(const Vector<T> &a, const Vector<T> &b) {
-    if ((a.ro2ws() != b.rows()) || (a.cols() != b.cols())) {
+    if ((a.rows() != b.rows()) || (a.cols() != b.cols())) {
       std::cerr << "MATRICES ARE NOT THE SAME SIZE!";
       exit(1);  // Exits the program
     }
